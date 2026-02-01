90b1317b4db47ede73e80db8b664b671f126ee8e
kyfex-uwu <kyfexuwu@gmail.com>
2026-02-01 15:25:12 -0700

    little bit of refactoring + trying to change the game icon

--- mod_main.gd
+++ mod_main.gd
@@ -1,11 +1,13 @@
 extends Node
 
 const MOD_NAME := "Multrapool-Cue" 
+const cue = preload("res://mods-unpacked/Multrapool-Cue/static.gd")
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
 
 func _init() -> void:
+
     mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_NAME)
     extensions_dir_path = mod_dir_path.path_join("extensions")
 
@@ -13,7 +15,8 @@ func _init() -> void:
     install_script_extensions()
     install_script_hook_files()
     
-    Multrapool_Cue.take_over(MOD_NAME, "data/balls_data/multrapool_logo.tres")
+    cue.take_over(MOD_NAME, "data/balls_data/multrapool_logo.tres")
+    cue.take_over(MOD_NAME, "icon.png")
 
     ModLoaderMod.add_translation(mod_dir_path.path_join("Multrapool.en.translation"))
     
@@ -23,7 +26,6 @@ func install_script_extensions() -> void:
     ModLoaderMod.install_script_extension(extensions_dir_path + "/main_menu.gd")
 
 func install_script_hook_files() -> void:
-    pass
     ModLoaderMod.add_hook(get_all_files_hook, "res://utils/utils.gd", "get_all_files_with_extension")
 
 func _ready() -> void:
@@ -35,7 +37,7 @@ func _ready() -> void:
 func get_all_files_hook(chain: ModLoaderHookChain, path: String, extension: String) -> Array[String]:
     var orig = chain.execute_next([path, extension])
     
-    for file_path in Multrapool_Cue.virtual_files:
+    for file_path in cue.virtual_files:
         if file_path.begins_with(path):
             orig.append(file_path)
     
